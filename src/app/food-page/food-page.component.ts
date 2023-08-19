b2feab04582cb8128f534365bccedf4d5b3de438
b.maboudi <behnazmaboudi@gmail.com>
2023-08-19 17:48:17 +0330

    change and create cardpage

--- src/app/food-page/food-page.component.ts
+++ src/app/food-page/food-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Food } from '../shared/models/food';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { FoodService } from '../services/food/food.service';
+import { CartService } from '../services/cart/cart.service';
 
 @Component({
   selector: 'app-food-page',
@@ -10,11 +11,16 @@ import { FoodService } from '../services/food/food.service';
 })
 export class FoodPageComponent implements OnInit {
   food!: Food;
-  constructor(private activatedRoute: ActivatedRoute,private foodService: FoodService) {
+  constructor(private activatedRoute: ActivatedRoute,private foodService: FoodService, private cartService: CartService, private router: Router ) {
     activatedRoute.params.subscribe((params) => {
       if (params['id'])
         this.food = foodService.getFoodById(params['id']);
     });
   }
   ngOnInit(): void {}
+
+  addToCart(){
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+  }
 }
---
+++ src/app/shared/models/cardItem.ts
@@ -0,0 +1,13 @@
+import { Food } from "./food";
+
+export class CartItem{
+    constructor(food: Food){
+        this.food = food;
+    }
+ food: Food;  
+ quantity: number = 1; 
+
+ getPrice():number{
+    return this.food.price * this.quantity;
+ }
+}
